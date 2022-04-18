36813b0e26a0bd39e0f6564c043d13c96668d9f4
abigailandrews22 <58536270+abigailandrews-projects@users.noreply.github.com>
2022-04-17 18:03:13 -0700

    Modal

--- thesis190/src/components/Book/Book.js
+++ thesis190/src/components/Book/Book.js
@@ -2,32 +2,32 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import style from "./Book.module.scss";
 
-export const Book = (i) => {
-  const [isClicked, setIsClicked] = useState(true);
+export const Book = (props) => {
   const variants = {
-    clicked: {
-      position: "sticky",
+    passed: {
+      position: "fixed",
       top: "400px",
-      x: `${-285 + i.i * 25}px`,
+      x: `${-285 + props.i * 25}px`,
       y: `-72px`,
     },
-    notclicked: {},
+    notpassed: {},
+  };
+  const handleClick = () => {
+    props.updateOpenModal(true);
   };
 
   return (
-    <motion.div
-      animate={isClicked ? "clicked" : "notclicked"}
-      variants={variants}
-    >
-      <div
-        onClick={() => {
-          setIsClicked((isClicked) => !isClicked);
-        }}
-        className={style.container}
+    <>
+      <motion.div
+        animate={props.passedModal ? "passed" : "notpassed"}
+        variants={variants}
       >
-        {i.i}
-      </div>
-    </motion.div>
+        <div onClick={handleClick} className={style.container}>
+          {props.i}
+          {props.passedModal}
+        </div>
+      </motion.div>
+    </>
   );
 };
 
